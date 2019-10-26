532137d8fe0e21d72c4d012d84296ce62f795f67
Anastasia Kishkun <Kichkun@yandex.ru>
2019-10-27 02:25:49 +0300

    solved

--- src/main/api/quotes.py
+++ src/main/api/quotes.py
@@ -3,6 +3,10 @@ from PIL import Image, ImageDraw, ImageFont
 import wikiquote
 import random
 import textwrap
+import sys
+import numpy as np
+sys.path.remove("/opt/ros/kinetic/lib/python2.7/dist-packages")
+import cv2
 
 
 def get_quote(name):
@@ -34,7 +38,7 @@ def generate_picture_with_text(name, text, path):
     image = image.resize((basewidth, hsize), Image.ANTIALIAS)
 
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype('Impact.ttf', size=45)
+    font = ImageFont.truetype('impact.ttf', size=45)
     (x, y) = (40, image.size[1] - lines_height)
     color = 'rgb(255, 255, 255)'
 
@@ -48,27 +52,26 @@ def generate_picture_with_text(name, text, path):
 
 def merge_images_vertically(first_image, second_image, name):
     images_list = [first_image, second_image]
-    imgs = [Image.open(i) for i in images_list]
 
-    min_img_width = min(i.width for i in imgs)
-    draw = ImageDraw.Draw(imgs[0])
-    font = ImageFont.truetype('Impact.ttf', size=25)
-    color = 'rgb(0, 0, 0)'
-    draw.text((50, 150), name + ':', fill=color, font=font, align="center")
+    imgs = [cv2.imread(i) for i in images_list]
+
+    min_img_width = min(i.shape[1] for i in imgs)
+    print(min_img_width, imgs[0].shape, imgs[1].shape)
+    
+    font = cv2.FONT_HERSHEY_TRIPLEX
+    cv2.putText(imgs[0], name + ':', (50, 180), font, 1.1, (0, 0, 0), 1)
 
     total_height = 0
     for i, img in enumerate(imgs):
-        if img.width > min_img_width:
-            imgs[i] = img.resize((min_img_width, int(img.height / img.width * min_img_width)), Image.ANTIALIAS)
-        total_height += imgs[i].height
-
-    img_merge = Image.new('RGB', (min_img_width, total_height))
-    y = 0
-    for img in imgs:
-        img_merge.paste(img, (0, y))
-
-        y += img.height
-    img_merge.save(name + 'merged.png')
+        if img.shape[1] > min_img_width:
+            imgs[i] = cv2.resize(img,(min_img_width, int(img.shape[0] / img.shape[1] * min_img_width)))
+        total_height += imgs[i].shape[0]
+    print(min_img_width, imgs[0].shape, imgs[1].shape)
+    merged = np.concatenate((imgs[0], imgs[1]), axis=0)
+    
+    
+    cv2.imwrite(name + 'merged.png', merged)
+    return 0
 
 
 name = 'Napoleon'
